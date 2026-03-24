3fac9847236200b33dc94d48b43866d0b184781c
Mayank Rathour <mrathour1997@gmail.com>
2026-03-24 12:44:40 +0530

    Djoser library configured

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'django_filter',
+    'LittleLemon',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
